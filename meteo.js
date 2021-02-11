cc3a2d38297847f4c464881fca300b65e4e4d928
rayhanebousnina <bousninarayhanesimplonenda@gmail.com>
2021-02-11 14:22:25 +0100

    finalisation

--- meteo.js
+++ meteo.js
@@ -1,28 +1,30 @@
 let villeChoisie = "sousse";
 recievedata(villeChoisie);
 
+// Pour choisir une autre ville
 let changerville = document.querySelector("#changerville");
 changerville.addEventListener("click", () => {
   villeChoisie = prompt("Quelle ville souhaitez-vous voir?");
   recievedata(villeChoisie);
 });
-
+// Fonction de récupération des données de type json
 function recievedata(city) {
   const url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     city +
-    "&appid=7ed8c5c726848a9dfc0fadbda23b25e6&units=metric";
+    "&appid=7ed8c5c726848a9dfc0fadbda23b25e6&units=metric"; // Url de l'api contenant l'api key
 
-  let requete = new XMLHttpRequest();
-  requete.open("GET", url);
-  requete.responseType = "json";
-  requete.send();
+  let requete = new XMLHttpRequest(); // Création d'un objet pour récupérer les données
+  requete.open("GET", url); // Récupération des données
+  requete.responseType = "json"; // La réponse est de type json
+  requete.send(); // L'envoie du requete
 
+  // Une fois la réponse est reçue, on lance cette fonction
   requete.onload = function () {
     if (requete.readyState === XMLHttpRequest.DONE) {
       if (requete.status === 200) {
         let response = requete.response;
-        console.log(response);
+        //console.log(response);
         let city = response.name;
         let temp = response.main.temp;
         let press = response.main.pressure;
